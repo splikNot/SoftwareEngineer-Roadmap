c3b33c9f10020fddcecd49cf7293b0b6c0878dd2
Raphael Cardoso <91639365+splikNot@users.noreply.github.com>
2025-02-13 19:53:02 -0300

    Add files via upload

---
+++ Cursos/UdemyJava/Tasks/Exercises/src/exercises/Employees.java
@@ -0,0 +1,22 @@
+package exercises;
+
+public class Employees {
+	public String name = "";
+	public double grossSalary = 0;
+	public double tax = 0;
+	
+	public double NetSalary() {
+		double netSalary = grossSalary - tax;
+		return netSalary;
+	}
+	
+	public void IncreseSalary(double percentage) {
+		double increase = grossSalary + (percentage/100)*grossSalary;
+		grossSalary = increase;
+	}
+	
+	public String toString() {
+		return name + ", $" + String.format("%.2f", NetSalary());
+	}
+	
+}
