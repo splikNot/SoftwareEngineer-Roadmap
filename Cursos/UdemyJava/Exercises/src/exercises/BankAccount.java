efa485a2fd8cbed2892b84cf137282fc66a64d40
Raphael Cardoso <91639365+splikNot@users.noreply.github.com>
2025-02-16 21:32:51 -0300

    Add files via upload

---
+++ Cursos/UdemyJava/Exercises/src/exercises/BankAccount.java
@@ -0,0 +1,39 @@
+package exercises;
+import java.util.Locale;
+
+
+public class BankAccount {
+	
+	static { Locale.setDefault(Locale.US); }
+	
+    private String holderName;
+    
+    private final int accountNumber;
+    
+    private double balanceAccount = 0.00;
+
+    //Constructor
+    public BankAccount(int accountNumber, String holderName, double inicialDeposit){
+        this.holderName = holderName;
+        this.accountNumber = accountNumber;
+        deposit(inicialDeposit);
+    }
+
+    // WithDraw
+    public void withDraw(double valueToDraw){
+    	final int withDrawTax = 5;
+        balanceAccount -= (valueToDraw + withDrawTax);
+    }
+
+    // Deposit
+    public void deposit(double valueToDeposit){
+        balanceAccount += valueToDeposit;
+        
+    }
+    //DataAccount;
+    public void dataAccount() {
+    	System.out.printf("Account %d, Holder: %s, Balance: $%.2f %n",
+    			accountNumber, holderName, balanceAccount);
+    }
+    
+}
